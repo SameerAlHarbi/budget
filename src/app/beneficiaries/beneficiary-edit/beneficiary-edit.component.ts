3bf18721554db1a664e11edf6225329bb73d5a07
Sameer Al Harbi <smrhrbi@gmail.com>
2019-10-30 22:49:37 +0300

    beneficiaries edit

--- src/app/beneficiaries/beneficiary-edit/beneficiary-edit.component.ts
+++ src/app/beneficiaries/beneficiary-edit/beneficiary-edit.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Beneficiary } from '../beneficiary.model';
 import { BeneficiariesService } from '../beneficiaries.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Relation } from 'src/app/relations/relation.model';
+import { RelationsService } from 'src/app/relations/relations.service';
+import { Subscription } from 'rxjs';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-beneficiary-edit',
@@ -14,12 +18,34 @@ export class BeneficiaryEditComponent implements OnInit {
   editingItemIndex: number;
   editingItem: Beneficiary;
 
+  relationsList: Relation[];
+  relationsSubscription: Subscription;
+
+  beneficiariesForm: FormGroup;
+
   constructor(private beneficiariesService: BeneficiariesService
-    , private router: Router
-    , private route: ActivatedRoute) { }
+            , private relationsService: RelationsService
+            , private router: Router
+            , private route: ActivatedRoute) { }
 
   ngOnInit() {
-    
+    this.relationsList = this.relationsService.getAllRelations();
+    this.relationsSubscription = this.relationsService.relationsChanged.subscribe(
+      (relations: Relation[]) => {
+        this.relationsList = relations;
+      });
+
+    this.beneficiariesForm = new FormGroup({
+        code: new FormControl('0099', Validators.required),
+        name: new FormControl(null, Validators.required),
+        relation: new FormControl('Brother', Validators.required),
+        mobile: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email])
+      });
+  }
+
+  onSubmit() {
+    console.log(this.beneficiariesForm);
   }
 
 }
